93a9cffa9a4d9f02ae21217f937104d143efa3c8
arwen.e.nicholson@gmail.com <arwen.e.nicholson@gmail.com@ba3ad000-c725-3ee3-f0a2-a6f435c0371d>
2011-09-27 06:49:40 +0000

    Updated the labjack_server.py. The variables have been neated and comments added. There is also a method to ensure the dome moves the shortest distance to it's destination, and the user can now either ask for the dome to move relative to it's current position, or tell the dome to move to a position relative to North.
    git-svn-id: https://macquarie-observatory-automation.googlecode.com/svn/trunk@21 ba3ad000-c725-3ee3-f0a2-a6f435c0371d

--- labjack/labjack_server.py
+++ labjack/labjack_server.py
@@ -18,24 +18,22 @@ import ei1050
 #   do different parts of the job. You's almost certainly *not* want to do 
 #   this.
 LJ=u3.U3()
-LJPROBE=ei1050.EI1050(LJ, enablePinNum=0, dataPinNum=1, clockPinNum=2)
+LJPROBE=ei1050.EI1050(LJ, enablePinNum=0, dataPinNum=1, clockPinNum=2) #Sets up the humidity probe
 LJ.configIO(NumberOfTimersEnabled = 2)
-LJ.getFeedback(u3.Timer0Config(8), u3.Timer1Config(8))
+LJ.getFeedback(u3.Timer0Config(8), u3.Timer1Config(8)) #Sets up the dome tracking wheel
 
 #***********************************************************************#
 #2) Now define our main class, the LabjackServer.
 class LabjackServer:
 
-#Variables not used yet...
-    dome_status = 'HOME'   
-    slit_status = 'closed'  
-    weather_status = 'NULL' #Needs to come from connection to weather server. Who is the client?
-    
 #Some properties relating to the relative encoder.
-    wheel_pos = 0.0
-    wheel_command = 0
-    wheel_moving = False
-    current_pos = 0 #MJI Doesn't understand the difference between WHEEL_POS, WHEEL_COMMAND and this... Needs comments
+    dome_command = 0 #this is the distance the user wants the dome to move to be in position
+    dome_moving = False #a variable to keep track of whether the dome is moving due to a remote user command
+    current_pos = 0 #The position that the dome is at right now
+    counts_per_degree = 11.82 #how many counts from the wheel encoder there is to a degree
+    slitoffset=53.83*counts_per_degree #position, in counts, of the slits when home switch is activated
+    counts_at_start = 0.0 #This will record the counts from the labjack before the dome starts to move to a new position
+			  #We need this to keep track of how far we have traveled.
 
 #*************************************************************************#
 #All the definitions used are listed here
@@ -58,58 +56,96 @@ class LabjackServer:
     
     def cmd_status(self,the_command):
         '''Return the status of the entire system.'''
-        return(str(self.wheel_pos))
+        return(str(self.current_pos/self.counts_per_degree)+" degrees.")
     
-    def cmd_wheel(self,the_command):
-        '''Move the wheel'''
-        commands=string.split(the_command)
-        if self.wheel_moving == True: 
-            return "Wheel already moving"
-        elif len(commands) ==1:
-            return "Wheel\'s current position: "+str(self.wheel_pos)+". Wheel NOT moving."
+    def cmd_dome(self,the_command):
+        '''Move the dome. Put a + or - before the number you input to move a certain distance
+	from the current position, or just input the postion number where you want the dome to
+	be positioned. Eg '+20' means 'move 20 degrees clockwise from current position'.
+	'-20' means 'move 20 degrees anticlockwise from current positon'. '20' means, move to
+	20 degrees from North (North being at the defined 0 degrees point). Please only input
+	integers. A decimal will not be read.'''
+        commands=str.split(the_command)
+        if self.dome_moving == True:
+            return "Dome moving, input only available when the dome is stationary."
+        elif len(commands) == 1:
+            return "Dome's current position: "+str(self.current_pos/self.counts_per_degree)+" degrees. Dome NOT moving."
         else:
-            self.wheel_command = commands[1]
-            self.wheel_moving = True
-            return "Wheel''s current position: "+str(self.wheel_pos)+". Moving Wheel."
+            self.counts_at_start=self.current_pos
+            self.dome_command = commands[1] #Grabs the number the user input
+	    temp = list(self.dome_command)  #we use the array 'temp' to make sure we have a correct input
+	    degree_move = 0.0
+	    newdegree_move = 0.0
+	    sign = 'null'   #This will keep track of whether we are moving in a clockwise or anticlockwise direction
+	    extra_turns = 0 #This will keep track of any extra turns the user input.. ie if they put 500 degrees move
+			    #that would be extra_turn = 1 as the dome only actually needs to move 140 degrees
+	    if temp[0] == '+' or temp[0] == '-': #user has asked to move a certain amount from where we are now
+		sign = temp[0]
+		del temp[0]
+	    else: 				#user has asked to go to a specific degree location
+		if self.dome_command.isdigit():
+		   distance = self.dome_command - self.dome_pos #where you want to go - where you are now to convert into
+		   temp = list(distance)                        #a distance to move from the current position
+		   if temp[0] == '-':  #we want to move clockwise
+			sign = '-'
+			del temp[0]
+		   else: sign = '+'  #we want to move anticlockwise
+		else: return "ERROR, invalid input"
+	    for i in range(len(temp)):
+		degree_move = degree_move + temp[i] #We put the array back together to make a complete number again
+	    if degree_move.isdigit():               #Checking if the user input a number
+		extra_turns = int(float(degree_move)/360) #Make sure we are only asking the dome to move less than one turn
+		reduced_degree_move = int(degree_move) - int(extra_turns)*360 #Update the degrees to move
+		if int(reduced_degree_move) > 180:
+		   minimised_degree_move = 360 - reduced_degree_move #This will minimise the motion of the dome rotation
+		   if temp[0] == '-': sign = '+'   		 #we need to change the direction we are moving in
+		   if temp[0] == '+': sign = '-'
+		if sign == '+': 
+		   self.dome_command = minimised_degree_move  #setting the variable to be sent to 'dome_rotate'
+		   return
+		elif sign == '-': 
+		   self.dome_command = str(-1*int(minimised_degree_move))
+		   return
+		else: return "ERROR, although I shouldn't be able to get here."
+	    else: return "ERROR, invalid input"
+            self.dome_moving = True #This will tell background task 'dome_location' to call task 'dome_moving'
+            return "Dome's current position: "+str(self.current_pos/self.counts_per_degree)+" degrees. Dome moving."
         
 #****** End of user commands *******
         
-#This is the background job that defines if the wheel is moving.
+#This is the background job that defines if the dome is moving.
     def dome_rotate(self,command):
-        command = float(command)
-        command = command*48   #turns number of turns into number of counts
-        temp = LJ.getFeedback(u3.QuadratureInputTimer())
-        temp2 = temp[-1]
-        temp2 = temp2 - (self.wheel_pos*48)
-        destination = command + self.wheel_pos
-        if command >= 0:
-            if temp2 < command:
-                self.wheel_moving = True
-                temp = LJ.getFeedback(u3.QuadratureInputTimer())
-                self.current_pos = float(temp[-1])
-            else: self.wheel_moving = False
-        elif command < 0:
-            if temp2 > command:
-                self.wheel_moving = True
-                temp = LJ.getFeedback(u3.QuadratureInputTimer())
-                self.current_pos = float(temp[-1])
-            else: self.wheel_moving = False
-        else: 
+        distance_to_travel = float(command) #distance in degrees
+        counts_to_travel = distance_to_travel*self.counts_per_degree #covert degrees to counts
+    	print "current_pos "+str(self.current_pos)
+        if counts_to_travel >= 0:  #clockwise motion
+            if self.counts_at_start+counts_to_travel > self.current_pos: #Check if we are in position yet
+                self.dome_moving = True
+        #print self.current_pos
+            else: self.dome_moving = False
+        elif counts_to_travel < 0: #counterclockwise motion
+            if self.counts_at_start+counts_to_travel < self.current_pos:
+                self.dome_moving = True
+            else: self.dome_moving = False
+        else:
             return "ERROR"
-        
-        if self.wheel_moving == False:
-            turns = (self.current_pos/48.0) - self.wheel_pos
-            pos = self.wheel_pos
-            pos = pos + turns
-            self.wheel_pos = pos
-            return turns
+        if self.dome_moving == False:
+    	    #print "Dome in position: "+str(self.current_pos/self.counts_per_degree)+" degrees."
+	    return "Dome in position: "+str(self.current_pos/self.counts_per_degree)+" degrees."
         else: return
-        
-#This will 'move' the wheel if there has been a command which has not yet completed
-#obviously at the moment the wheel is moved manually
-    def wheel_move(self):
-        if self.wheel_moving == True:
-            self.dome_rotate(self.wheel_command)
-                
+       
+#This will 'move' the dome if there has been a command which has not yet completed
+#obviously at the moment the dome is moved manually
+    def dome_location(self):
+    	temp = LJ.getFeedback(u3.QuadratureInputTimer()) #This will constantly update the current position of the dome
+    	self.current_pos = float(temp[-1])		 #This enables us to keep track even if the dome is manually moved
+	f = open('dome_position.dat','w')
+	f.write("Dome is in position "+str(self.current_pos/self.counts_per_degree)+" degrees.")
+	f.close()
+        if self.dome_moving == True:
+            self.dome_rotate(str(self.dome_command))
+               
+
+
 
 
