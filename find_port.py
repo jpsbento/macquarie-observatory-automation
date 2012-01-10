29068019a7cc0b9d2cbc772b4dedabff5c7c5e8d
arwen.e.nicholson@gmail.com <arwen.e.nicholson@gmail.com@ba3ad000-c725-3ee3-f0a2-a6f435c0371d>
2012-01-10 00:13:51 +0000

    automatic assigning of ports to devices based on one easy to edit file now complete
    git-svn-id: https://macquarie-observatory-automation.googlecode.com/svn/trunk@86 ba3ad000-c725-3ee3-f0a2-a6f435c0371d

--- find_port.py
+++ find_port.py
@@ -4,8 +4,9 @@ class FindPort:
 
 	def findPort(self, device):
 		self.device = device
-		try: devicefile = open('device_list.txt')
-		except Exception: return 'ERROR file "device_list.txt" not found'
+
+                devicefile = open('/home/phy-admin/macquarie-observatory-automation/device_list.txt','r')
+	#	except Exception: print 'ERROR file "device_list.txt" not found'
 		device_list = devicefile.readlines()
 		devicefile.close()
 		Port = ''
@@ -14,9 +15,9 @@ class FindPort:
 			items = str.split(device_list[0])
 			for i in range(0, len(items)):
 				if items[i] == 'Port': Port_column = i
-				#print items[i]
-		else: return 'ERROR 1'
-		if IP_column == '' or Port_column == '': return 'ERROR 2'
+				print items[i]
+		else: print 'ERROR 1'
+		if Port_column == '': print 'ERROR 2'
 		for line in device_list:
 			item = str.split(line)
 			if item[0][0] != '#' and device == item[0]:
