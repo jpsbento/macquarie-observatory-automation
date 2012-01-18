18d23cc6b3eac730d3c462773569d2522db9332a
ajhea3@gmail.com <ajhea3@gmail.com@ba3ad000-c725-3ee3-f0a2-a6f435c0371d>
2012-01-18 05:23:24 +0000

    sbig camera, temperature commands, input in degrees C
    git-svn-id: https://macquarie-observatory-automation.googlecode.com/svn/trunk@95 ba3ad000-c725-3ee3-f0a2-a6f435c0371d

--- sbig/sbigudrv_server.py
+++ sbig/sbigudrv_server.py
@@ -1,4 +1,5 @@
 import sbigudrv as sb
+import numpy as np
           
 sb.SBIGUnivDrvCommand(sb.CC_OPEN_DRIVER, None,None)
 
@@ -18,20 +19,53 @@ class SBigUDrv:
 	def cmd_setTemperature(self,the_command):
 		commands = str.split(the_command)
 		if len(commands) < 2 : return 'error: no input value'
-		b = self.sb.SetTemperatureRegulationParams()
-		b.Regulation = 1
-		try: temp = int(commands[1])
+		b = sb.SetTemperatureRegulationParams()
+		b.regulation = 1
+		try: tempC = int(commands[1])
 		except Exception: return 'invalid input'
+		v1 = (3.0 * np.exp((0.943906 * (25.0 -tempC))/25.0))
+		temp = int(4096.0/((10.0/v1) + 1.0))
 		b.ccdSetpoint = temp
-		self.sb.SBIGUnivDrvCommand(sb.CC_SET_TEMPERATURE_REGULATION, b, None)
-		a = self.sb.QueryTemperatureStatusResults()
-		self.sb.SBIGUnivDrvCommand(sb.CC_QUERY_TEMPERATURE_STATUS,None,a)
-		return 'fan power = '+str(a.power) + ', CCD set point = ' + str(a.ccdSetpoint) +', current CCD temp = ' + str(a.ccdThermistor)
+		sb.SBIGUnivDrvCommand(sb.CC_SET_TEMPERATURE_REGULATION, b, None)
+		a = sb.QueryTemperatureStatusResults()
+		sb.SBIGUnivDrvCommand(sb.CC_QUERY_TEMPERATURE_STATUS,None,a)
+		SPa = a.ccdThermistor
+		v2 = 4096.0/SPa	
+		v3 = 10.0/(v2-1.0)
+		ccdThermistorC = 25.0 - 25.0 *((np.log(v3/3.0))/0.943906)
+		return 'regulation = ' +str(b.regulation) +', power = '+str(a.power) + ', CCD set point (A/D) = ' + str(a.ccdSetpoint) + ', CCD set point (C) = ' + str(tempC)+', current CCD temp (A/D) = ' + str(a.ccdThermistor) + ', current CCD temp (C) = ' + str(ccdThermistorC)
 		
 	def cmd_checkTemperature(self,the_command):
-		a = self.sb.QueryTemperatureStatusResults()
-		self.sb.SBIGUnivDrvCommand(sb.CC_QUERY_TEMPERATURE_STATUS,None,a)
-		return 'fan power = '+str(a.power) + ', CCD set point = ' + str(a.ccdSetpoint) +', current CCD temp = ' + str(a.ccdThermistor)
+		a = sb.QueryTemperatureStatusResults()
+		sb.SBIGUnivDrvCommand(sb.CC_QUERY_TEMPERATURE_STATUS,None,a)
+		SP = a.ccdSetpoint
+		v1 = 4096.0/SP
+		v2 = 10.0/(v1-1.0)
+		setPointC = int(25.0 - 25.0 *((np.log(v2/3.0))/0.943906))
+		
+		v3 = 4096.0/a.ccdThermistor
+		v4 = 10.0/(v3-1.0)
+		TempC = 25.0 - 25.0 *((np.log(v4/3.0))/0.943906)
+		
+		return 'regulation = ' +str(a.enabled) +', power = '+str(a.power) + ', CCD set point (A/D) = ' + str(a.ccdSetpoint) + ', CCD set point (C) = ' + str(setPointC)+', current CCD temp (A/D) = ' + str(a.ccdThermistor) + ', current CCD temp (C)' + str(TempC)
+
+	def cmd_disableRegulation(self,the_command):
+		b = sb.SetTemperatureRegulationParams()
+		b.regulation = 0
+		b.ccdSetpoint = 1000
+		sb.SBIGUnivDrvCommand(sb.CC_SET_TEMPERATURE_REGULATION, b, None)
+		a = sb.QueryTemperatureStatusResults()
+		sb.SBIGUnivDrvCommand(sb.CC_QUERY_TEMPERATURE_STATUS,None,a)
+		
+		v1 = 4096.0/b.ccdSetpoint
+		v2 = 10.0/(v1-1.0)
+		setPointC = int(25.0 - 25.0 *((np.log(v2/3.0))/0.943906))
+		
+		v3 = 4096.0/a.ccdThermistor
+		v4 = 10.0/(v3-1.0)
+		TempC = 25.0 - 25.0 *((np.log(v4/3.0))/0.943906)
+		
+		return 'regulation = ' +str(b.regulation) +', power = '+str(a.power) + ', CCD set point (A/D) = ' + str(a.ccdSetpoint) + ', CCD set point (C) = ' + str(setPointC)+', current CCD temp (A/D) = ' + str(a.ccdThermistor) + ', current CCD temp (C)' + str(TempC)
 
 			
 		
