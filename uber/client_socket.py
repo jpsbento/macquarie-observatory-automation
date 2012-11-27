f573c61bae1e5d9d16e90947a459ca84632d27d3
Joao Bento <jpsbento@gmail.com>
2012-11-28 10:32:56 +1100

    Some housekeeping

--- uber/client_socket.py
+++ uber/client_socket.py
@@ -10,7 +10,7 @@ class ClientSocket:
 		#Set up the class properties "server", "input", "hardware_name" and "hardware_object"
 		self.device = device
 		try: devicefile = open('device_list.txt')
-		except Exception: print 'ERROR file "device_list.txt" not found'
+		except Exception: print 'ERROR: file "device_list.txt" not found'
 		device_list = devicefile.readlines()
 		devicefile.close()
 		IP = ''
@@ -24,14 +24,14 @@ class ClientSocket:
 				if items[i] == 'Port': Port_column = i
 				#print items[i]
 		else: return 'ERROR 1'
-		if IP_column == '' or Port_column == '': print 'ERROR 2'
+		if IP_column == '' or Port_column == '': print 'ERROR: Device file with unknown format'
 		for line in device_list:
 			item = str.split(line)
 			if item[0][0] != '#' and device == item[0]:
 				try:
 					IP = item[IP_column]
 					Port = int(item[Port_column])
-				except Exception: print 'ERROR3'
+				except Exception: print 'ERROR: Unable to set IP and Port columns'
 		ADS = (IP,Port)
 		try:
 			self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,7 +39,7 @@ class ClientSocket:
 			self.client.settimeout(600)
 			self.client.setsockopt(1, 2, 1)
 			self.client.setsockopt(6, 1, 1)
-		except Exception: print 'ERROR4'
+		except Exception: print 'ERROR: Could not connect to server responsible for '+self.device+'. Please check that the server is running.'
 
 
 
@@ -48,7 +48,7 @@ class ClientSocket:
 		try: self.client.send(command)
 		except Exception: return 'Error sending command, connection likely lost.'
 		try: return self.client.recv(5000)
-		except Exception: return 'Error reciving response'
+		except Exception: return 'Error receiving response'
 
 
 
