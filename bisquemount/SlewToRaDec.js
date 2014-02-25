b4fde816a2f249caa66afb9b45e897be5eb6175f
Joao Bento <jpsbento@gmail.com>
2014-02-25 16:50:24 +1100

    Added a function to force a restart and reconnect to any of the linux machine servers that may have died

--- bisquemount/SlewToRaDec.js
+++ bisquemount/SlewToRaDec.js
@@ -1,7 +1,7 @@
 /* Java Script */
 
-var TargetRa = "6.399202408675837";
-var TargetDec = "-52.69556770840383";
+var TargetRa = "22.137251581561962";
+var TargetDec = "-46.96155608951899";
 var Out;
 
 sky6RASCOMTele.Connect();
